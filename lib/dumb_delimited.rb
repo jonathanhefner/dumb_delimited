66f035e25d2f55dfdf7a88acf633947f26eb0d34
Jonathan Hefner <jonathan@hefner.pro>
2019-05-16 13:00:54 -0500

    Tweak method docs

--- lib/dumb_delimited.rb
+++ lib/dumb_delimited.rb
@@ -6,11 +6,12 @@ require_relative "dumb_delimited/version"
 module DumbDelimited
 
   # Returns a model class for delimited data consisting of the specified
-  # columns.  The returned class inherits from Ruby's
-  # {https://ruby-doc.org/core/Struct.html +Struct+}, allowing data
-  # manipulation via accessor methods, via indexing by column name, and
-  # via indexing by column number.  See {ClassMethods} and
-  # {InstanceMethods} for the IO methods the returned class provides.
+  # +columns+.  The returned class inherits from Ruby's
+  # {https://docs.ruby-lang.org/en/trunk/Struct.html +Struct+}, allowing
+  # data manipulation via accessor methods, via indexing by column name,
+  # and via indexing by column number.  See {ClassMethods} and
+  # {InstanceMethods} for the additional methods the returned class
+  # provides.
   #
   # @example
   #   class Product < DumbDelimited[:sku, :name, :base_price, :sale_price]
@@ -19,10 +20,11 @@ module DumbDelimited
   #     end
   #   end
   #
+  # @example
   #   Customer = DumbDelimited[:name, :email, :address]
   #
-  # @param columns [*Symbol]
-  # @return [Class]
+  # @param columns [Array<Symbol>]
+  # @return [Class<Struct>]
   def self.[](*columns)
     Struct.new(*columns) do
       extend DumbDelimited::ClassMethods
@@ -35,14 +37,15 @@ end
 
 module DumbDelimited::ClassMethods
 
-  # Returns the advanced options Hash.  The Hash is not +dup+ed and can
-  # be modified directly.  Any modifications will be applied to all
-  # future IO operations for the model class.  For detailed information
-  # about available options, see Ruby's
-  # {http://ruby-doc.org/stdlib/libdoc/csv/rdoc/CSV.html#method-c-new
-  # CSV module}.
+  # Returns the CSV options Hash.  The Hash is not +dup+ed and can be
+  # modified directly.  Any modifications will be applied to all future
+  # IO operations for the model class.
   #
-  # @return [Hash]
+  # For detailed information about available options, see Ruby's
+  # {https://docs.ruby-lang.org/en/trunk/CSV.html#method-c-new CSV
+  # class}.
+  #
+  # @return [Hash<Symbol, Object>]
   def options
     @options ||= {
       col_sep: ",",
@@ -51,20 +54,24 @@ module DumbDelimited::ClassMethods
     }
   end
 
-  # Sets the advanced options Hash.  The entire Hash is replaced, and
-  # the new value will be applied to all future IO operations for the
-  # model class.  To set options individually, see {options}.  For
-  # detailed information about available options, see Ruby's
-  # {http://ruby-doc.org/stdlib/libdoc/csv/rdoc/CSV.html#method-c-new
-  # CSV module}.
+  # Sets the CSV options Hash.  The entire Hash is replaced, and the new
+  # values will be applied to all future IO operations for the model
+  # class.  To set options individually, see {options}.
   #
-  # @param o [Hash]
-  def options=(o)
-    @options = o
+  # For detailed information about available options, see Ruby's
+  # {https://docs.ruby-lang.org/en/trunk/CSV.html#method-c-new CSV
+  # class}.
+  #
+  # @param opts [Hash<Symbol, Object>]
+  # @return [Hash<Symbol, Object>]
+  def options=(opts)
+    @options = opts
   end
 
   # Returns the column delimiter used in IO operations.  Defaults to a
   # comma (<code>","</code>).
+  #
+  # Equivalent to <code>options[:col_sep]</code>.
   #
   # @return [String]
   def delimiter
@@ -76,6 +83,8 @@ module DumbDelimited::ClassMethods
   # delimiter can be safely chosen, and all IO operations will quote
   # field values as necessary.
   #
+  # Equivalent to <code>options[:col_sep] = delim</code>.
+  #
   # @example
   #   Point = DumbDelimited[:x, :y, :z]
   #   p = Point.new(1, 2, 3)
@@ -83,9 +92,10 @@ module DumbDelimited::ClassMethods
   #   Point.delimiter = "|"
   #   p.to_s  # == "1|2|3"
   #
-  # @param d [String]
-  def delimiter=(d)
-    self.options[:col_sep] = d
+  # @param delim [String]
+  # @return [String]
+  def delimiter=(delim)
+    self.options[:col_sep] = delim
   end
 
   # Parses a single delimited line into a model object.
@@ -95,7 +105,7 @@ module DumbDelimited::ClassMethods
   #   Point.parse_line("1,2,3")  # == Point.new(1, 2, 3)
   #
   # @param line [String]
-  # @return [self]
+  # @return [Struct]
   def parse_line(line)
     self.new(*CSV.parse_line(line, self.options))
   end
@@ -112,17 +122,17 @@ module DumbDelimited::ClassMethods
   #     #    ]
   #
   # @param text [String]
-  # @return [Array<self>]
+  # @return [Array<Struct>]
   def parse_text(text)
     # using CSV.new.each instead of CSV.parse to avoid unnecessary mass
     # memory allocation and deallocation
     CSV.new(text, self.options).each.map{|row| self.new(*row) }
   end
 
-  # Parses an entire delimited file into an array of model objects.
-  # This will load the entire contents of the file into memory, and may
-  # not be suitable for large files.  To iterate over file contents
-  # without loading it all into memory at once, use {each_in_file}.
+  # Parses a file into an array of model objects.  This will load the
+  # entire contents of the file into memory, and may not be suitable for
+  # large files.  To iterate over file contents without loading it all
+  # into memory at once, use {each_in_file}.
   #
   # @example
   #   # CONTENTS OF FILE "points.csv":
@@ -139,22 +149,27 @@ module DumbDelimited::ClassMethods
   #     #    ]
   #
   # @param path [String, Pathname]
-  # @return [Array<self>]
+  # @return [Array<Struct>]
   def parse_file(path)
     each_in_file(path).to_a
   end
 
-  # Iterates over a delimited file, parsing one row at a time into model
-  # objects.  This avoids loading the entire contents of the file into
-  # memory at once.  If a block is given, it will be passed a model
-  # object for each row in the file.  Otherwise, if a block is not
-  # given, an Enumerator will be returned.  Note that some Enumerator
-  # methods, such as +Enumerator#to_a+, will cause the entire contents
-  # of the file to be loaded into memory regardless.
+  # Parses a file one line at a time, yielding a model object for each
+  # line.  This avoids loading the entire contents of the file into
+  # memory at once.
   #
-  # @param path [String, Pathname]
-  # @yieldparam [self] current model object
-  # @return [Enumerator<self>, nil]
+  # An Enumerator is returned if no block is given.  Note that some
+  # Enumerator methods, such as +Enumerator#to_a+, can cause the entire
+  # contents of the file to be loaded into memory.
+  #
+  # @overload each_in_file(path, &block)
+  #   @param path [String, Pathname]
+  #   @yieldparam model [Struct]
+  #   @return [void]
+  #
+  # @overload each_in_file(path)
+  #   @param path [String, Pathname]
+  #   @return [Enumerator<Struct>]
   def each_in_file(path)
     return to_enum(__method__, path) unless block_given?
 
@@ -169,7 +184,7 @@ end
 module DumbDelimited::InstanceMethods
 
   # Serializes a model object to a delimited string, using the delimiter
-  # specified by {ClassMethods.delimiter}.
+  # specified by {ClassMethods#delimiter}.
   #
   # @return [String]
   def to_s
@@ -177,12 +192,13 @@ module DumbDelimited::InstanceMethods
   end
 
   # Serializes a model object to a delimited string, using the delimiter
-  # specified by {ClassMethods.delimiter}, and appends the string plus a
+  # specified by {ClassMethods#delimiter}, and appends the string plus a
   # row separator to the specified file.  Returns the model object.
-  # This method is convenient when working with single model objects,
-  # for example, appending a single entry to a log file.  However, it is
-  # not recommended for use with an array of model objects, due to the
-  # overhead of opening and closing the file for each append.
+  #
+  # This method is convenient when working with single model objects.
+  # For example, when appending a single entry to a log file.  However,
+  # it is not recommended for use with an array of model objects due to
+  # the overhead of opening and closing the file for each append.
   #
   # @example
   #   Point = DumbDelimited[:x, :y, :z]
@@ -192,7 +208,7 @@ module DumbDelimited::InstanceMethods
   #   File.read("out.txt")                          # == "1,2,3\n4,5,6\n"
   #
   # @param file [String, Pathname]
-  # @return [self]
+  # @return [Struct]
   def append_to_file(file)
     CSV.generate_line(self, self.class.options).append_to_file(file)
     self
