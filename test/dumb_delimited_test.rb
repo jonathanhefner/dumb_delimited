4750b9d67beaa88e48816ae20197d99585c26ed1
Jonathan Hefner <jonathan@hefner.pro>
2019-05-17 13:25:42 -0500

    Rewrite tests

--- test/dumb_delimited_test.rb
+++ test/dumb_delimited_test.rb
@@ -3,97 +3,142 @@ require "test_helper"
 class DumbDelimitedTest < Minitest::Test
 
   def setup
-    Row.options[:col_sep] = ","
+    @original_options = Row.options.dup
+  end
+
+  def teardown
+    Row.options = @original_options
   end
 
   def test_that_it_has_a_version_number
     refute_nil ::DumbDelimited::VERSION
   end
 
-  def test_creation
-    hash = make_hash
-    row = make_row_from_hash(hash)
-
-    assert_hash_and_row_match hash, row
+  def test_inherits_from_struct
+    assert_kind_of Struct, Row.new
   end
 
-  def test_line_round_trip
-    hash = make_hash
-    row = make_row_from_hash(hash)
-    row_again = Row.parse_line(row.to_s)
-
-    assert_hash_and_row_match hash, row_again
+  def test_members
+    assert_equal COLUMNS, Row.members
   end
 
-  def test_text_round_trip
-    hashes = 3.times.map{|i| make_hash(i) }
-    rows = hashes.map{|h| make_row_from_hash(h) }
-    rows_again = Row.parse_text(rows.join("\n"))
+  def test_initializer
+    values = make_values("foo")
+    row = Row.new(*values)
 
-    assert_hashes_and_rows_match hashes, rows_again
+    assert_equal values, row.to_a
   end
 
-  def test_file_round_trip
-    make_hashes_then_write_to_file_then do |hashes, path|
-      rows_again = Row.parse_file(path)
+  def test_options_attr
+    expected = Row.options.merge(col_sep: "#{__method__}")
+    Row.options = expected
 
-      assert_hashes_and_rows_match hashes, rows_again
-    end
+    assert_equal expected, Row.options
   end
 
-  def test_file_iteration_with_block
-    make_hashes_then_write_to_file_then do |hashes, path|
-      rows_again = []
-      Row.each_in_file(path) do |row|
-        rows_again << row
-      end
-
-      assert_hashes_and_rows_match hashes, rows_again
-    end
+  def test_options_defaults
+    assert_equal ",", Row.options[:col_sep]
+    assert_equal true, Row.options[:skip_blanks]
+    assert_equal :numeric, Row.options[:converters]
   end
 
-  def test_file_iteration_without_block
-    make_hashes_then_write_to_file_then do |hashes, path|
-      enum = Row.each_in_file(path)
-      assert enum.is_a?(Enumerable)
+  def test_options_is_mutable
+    Row.options[:col_sep] = "#{__method__}"
 
-      rows_again = enum.to_a
-      assert_hashes_and_rows_match hashes, rows_again
-    end
+    assert_equal "#{__method__}", Row.options[:col_sep]
+  end
+
+  def test_options_mutations_are_isolated
+    another_class = DumbDelimited[*COLUMNS]
+    another_class.options[:col_sep] = "#{__method__}"
+
+    refute_equal "#{__method__}", Row.options[:col_sep]
   end
 
   def test_delimiter_attribute
-    Row.delimiter = "expected"
-    assert_equal "expected", Row.delimiter
+    Row.delimiter = "#{__method__}"
+
+    assert_equal "#{__method__}", Row.delimiter
+    assert_equal "#{__method__}", Row.options[:col_sep]
   end
 
-  def test_arbitrary_delimiter
-    Row.delimiter = "!@#$%"
-    hash = make_hash
-    row = make_row_from_hash(hash)
+  def test_to_s
+    values = make_values("foo")
+    with_various_delimiters do
+      line = Row.new(*values).to_s
 
-    line = row.to_s
-    assert_equal (COLUMNS.length - 1), line.scan(Row.delimiter).length
+      assert_equal (COLUMNS.length - 1), line.scan(Row.delimiter).length
+      values.each do |value|
+        assert_includes line, value
+      end
+    end
+  end
 
-    row_again = Row.parse_line(line)
-    assert_hash_and_row_match hash, row_again
+  def test_parse_line
+    row = Row.new(*make_values("foo"))
+    with_various_delimiters do
+      assert_equal row, Row.parse_line(row.to_s)
+    end
+  end
+
+  def test_parse_text
+    rows = (1..3).map{|id| Row.new(*make_values(id)) }
+    with_various_delimiters do
+      assert_equal rows, Row.parse_text(rows.join("\n"))
+    end
+  end
+
+  def test_parse_file
+    rows = (1..3).map{|id| Row.new(*make_values(id)) }
+    with_various_delimiters do
+      write_rows_then(rows) do |path|
+        assert_equal rows, Row.parse_file(path)
+      end
+    end
+  end
+
+  def test_each_in_file_with_block
+    rows = (1..3).map{|id| Row.new(*make_values(id)) }
+    with_various_delimiters do
+      write_rows_then(rows) do |path|
+        each_rows = []
+        Row.each_in_file(path) do |row|
+          each_rows << row
+        end
+
+        assert_equal rows, each_rows
+      end
+    end
+  end
+
+  def test_each_in_file_without_block
+    rows = (1..3).map{|id| Row.new(*make_values(id)) }
+    with_various_delimiters do
+      write_rows_then(rows) do |path|
+        enum = Row.each_in_file(path)
+
+        assert enum.is_a?(Enumerable)
+        assert_equal rows, enum.to_a
+      end
+    end
   end
 
   def test_append_to_file
-    hashes = 3.times.map{|i| make_hash(i) }
-    rows = hashes.map{|h| make_row_from_hash(h) }
-
     Dir.mktmpdir do |dir|
       all_at_once = dir.to_pathname + "all_at_once"
       one_by_one = dir.to_pathname + "one_by_one"
 
-      rows.write_to_file(all_at_once)
-      rows.each{|r| r.append_to_file(one_by_one) }
+      rows = (1..3).map{|id| Row.new(*make_values(id)) }
+      with_various_delimiters do
+        rows.write_to_file(all_at_once)
+        rows.each{|row| row.append_to_file(one_by_one) }
 
-      assert_equal File.read(all_at_once), File.read(one_by_one)
+        assert_equal File.read(all_at_once), File.read(one_by_one)
+      ensure
+        File.delete(one_by_one)
+      end
     end
   end
-
 
   private
 
@@ -101,38 +146,22 @@ class DumbDelimitedTest < Minitest::Test
 
   Row = DumbDelimited[*COLUMNS]
 
-  def make_hash(identifier = nil)
-    COLUMNS.reduce({}) do |hash, col|
-      hash.merge(col => "__#{col}__#{identifier}")
+  def make_values(id)
+    COLUMNS.map{|col| "row_#{id}_col_#{col}" }
+  end
+
+  def with_various_delimiters(&block)
+    [nil, "\t", "!@#$%"].each do |delimiter|
+      Row.delimiter = delimiter if delimiter
+      block.call
     end
   end
 
-  def make_row_from_hash(hash)
-    Row.new(*COLUMNS.map{|c| hash[c] })
-  end
-
-  def make_hashes_then_write_to_file_then(&block)
+  def write_rows_then(rows, &block)
     Dir.mktmpdir do |dir|
       path = dir.to_pathname + "file"
-      hashes = 13.times.map{|i| make_hash(i) }
-      rows = hashes.map{|h| make_row_from_hash(h) }
       rows.write_to_file(path)
-      block.call(hashes, path)
-    end
-  end
-
-  def assert_hash_and_row_match(hash, row)
-    COLUMNS.each_with_index do |col, i|
-      assert_equal hash[col], row[i], "Bad value in row at index #{i}"
-      assert_equal hash[col], row[col], "Bad value in row at key #{col}"
-      assert_equal hash[col], row.send(col), "Bad value in row from attribute #{col}"
-    end
-  end
-
-  def assert_hashes_and_rows_match(hashes, rows)
-    assert_equal hashes.length, rows.length, "Different number of hashes and rows"
-    hashes.zip(rows).each do |h, r|
-      assert_hash_and_row_match h, r
+      block.call(path)
     end
   end
 
